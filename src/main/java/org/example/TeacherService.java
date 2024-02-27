c56c1606868406610be883730b995cd99f0e5d18
Irina Soldatova <soldatovaik@yandex.ru>
2024-02-27 15:15:03 +0300

    teacher service

--- src/main/java/org/example/TeacherService.java
+++ src/main/java/org/example/TeacherService.java
@@ -1,13 +1,30 @@
 package org.example;
 
-public class TeacherService {
-    private Teacher teacher;
 
-    public TeacherService(Teacher teacher) {
-        this.teacher = teacher;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TeacherService {
+    private TeacherList teacherList;
+
+    public TeacherService(TeacherList teacherList) {
+        this.teacherList = teacherList;
     }
 
-    public Teacher getTeacher() {
-        return teacher;
+    public TeacherList getTeacherList() {
+        return teacherList;
+    }
+
+    public List<Teacher> getSortedTeacherList() {
+        List<Teacher> sortedTeacher = new ArrayList<>(teacherList.getTeacherList());
+        Collections.sort(sortedTeacher);
+        return sortedTeacher;
+    }
+
+    public List<Teacher> getSortedTeacherByFIO(){
+        List<Teacher> teachers = new ArrayList<>(teacherList.getTeacherList());
+        teacherList.getTeacherList().sort(new UserComparator<Teacher>());
+        return teachers;
     }
 }
